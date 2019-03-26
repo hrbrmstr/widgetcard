c13c7b7e5142566bc6e94978fdb54084b517c154
hrbrmstr <bob@rud.is>
2019-03-26 09:57:57 -0400

    R package repo initialization complete

---
+++ R/widgetcard-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/widgetcard>
+#' - BugReports: <https://gitlab.com/hrbrmstr/widgetcard/issues>
+#' 
+#' @md
+#' @name widgetcard
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
